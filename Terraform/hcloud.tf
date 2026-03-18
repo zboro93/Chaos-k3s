7654e948192f669c04c84be98a3936585b455736
Zboro93 <lukasz.0925@gmail.com>
2026-03-18 14:20:36 +0000

    upated hcloud.tf

--- Terraform/hcloud.tf
+++ Terraform/hcloud.tf
@@ -12,29 +12,26 @@ data "hcloud_ssh_key" "chaos_ssh_key" {
   name = "chaos-k3s"
 }
 
-
-#Create primary IP
-resource "hcloud_primary_ip" "primary_ip_1" {
-  name          = "primary_ip_1"
-  location      = "nbg1"
-  type          = "ipv4"
-  assignee_type = "server"
-  auto_delete   = true
-}
-
-# Create a server
+# Create a k3s master node server
 resource "hcloud_server" "k3s-master-node" {
   name        = "k3s-master-node"
   image       = "ubuntu-24.04"
   server_type = "cpx22"
   location    = "nbg1"
   ssh_keys    = [data.hcloud_ssh_key.chaos_ssh_key.id]
-  public_net {
-    ipv4_enabled = true
-    ipv4         = hcloud_primary_ip.primary_ip_1.id
-    ipv6_enabled = false
-  }
 }
+
+
+# Create the k3s worker nodes
+resource "hcloud_server" "k3s-worker-nodes" {
+  count       = 2
+  name        = "k3s-worker-node${count.index + 1}"
+  image       = "ubuntu-24.04"
+  server_type = "cpx22"
+  location    = "nbg1"
+  ssh_keys    = [data.hcloud_ssh_key.chaos_ssh_key.id]
+}
+
 
 output "k3s-master-node-ip" {
   value = hcloud_server.k3s-master-node.ipv4_address
