15fad0070359294aefec8678289b5d287959ce68
Zboro93 <lukasz.0925@gmail.com>
2026-03-18 12:46:40 +0000

    updated hcloud.tf

--- Terraform/hcloud.tf
+++ Terraform/hcloud.tf
@@ -4,10 +4,38 @@ variable "hcloud_token" {}
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token = "${var.hcloud_token}"
+  token = var.hcloud_token
+}
+
+# Use existing ssh_key
+data "hcloud_ssh_key" "chaos_ssh_key" {
+  name = "chaos-k3s"
+}
+
+
+#Create primary IP
+resource "hcloud_primary_ip" "primary_ip_1" {
+  name          = "primary_ip_1"
+  location      = "nbg1"
+  type          = "ipv4"
+  assignee_type = "server"
+  auto_delete   = true
 }
 
 # Create a server
-resource "hcloud_server" "web" {
-  # ...
+resource "hcloud_server" "k3s-master-node" {
+  name        = "k3s-master-node"
+  image       = "ubuntu-24.04"
+  server_type = "cpx22"
+  location    = "nbg1"
+  ssh_keys    = [data.hcloud_ssh_key.chaos_ssh_key.id]
+  public_net {
+    ipv4_enabled = true
+    ipv4         = hcloud_primary_ip.primary_ip_1.id
+    ipv6_enabled = false
+  }
+}
+
+output "k3s-master-node-ip" {
+  value = hcloud_server.k3s-master-node.ipv4_address
 }
