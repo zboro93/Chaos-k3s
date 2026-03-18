d4c2002ab7a5f5139a572065cdc8b1886d0a27f7
Zboro93 <lukasz.0925@gmail.com>
2026-03-18 11:02:35 +0000

    initial terraform config

--- Terraform/hcloud.tf
+++ Terraform/hcloud.tf
@@ -1,1 +1,13 @@
+# Set the variable value in *.tfvars file
+# or using the -var="hcloud_token=..." CLI option
+variable "hcloud_token" {}
 
+# Configure the Hetzner Cloud Provider
+provider "hcloud" {
+  token = "${var.hcloud_token}"
+}
+
+# Create a server
+resource "hcloud_server" "web" {
+  # ...
+}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+    }
+  }
+  required_version = ">= 0.13"
+}
